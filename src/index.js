5a7d8d2a45f094a4fcfca4909dd5f8bdac4a50d9
Vidyasagar7 <vidsagar1999@gmail.com>
2021-03-13 14:27:20 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
+const app = express();
+
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send(ads);
+});
+
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
